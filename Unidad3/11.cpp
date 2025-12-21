2585fc20f047913da657b79a65e415dce1ff9d1b
ashli castillo valdez <yoshi099123@gmail.com>
2025-12-21 14:26:22 -0800

    Actualización del proyecto

--- Unidad3/11.cpp
+++ Unidad3/11.cpp
@@ -22,13 +22,10 @@ int main(){
 
         for (int i = 0; i <3; i++){
             for (int j = 0; j < 3; j++){
-                
                 cout << "[" << matriz[i][j] << "] ";
             }
             cout << endl;
         }
 
         cout << "La suma es: " << suma;
-
-
 }
